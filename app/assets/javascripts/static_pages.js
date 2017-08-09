9945a47a9be4a49b5a65cb5bfcefe693c7a1bf12
isarceaux <isarceaux>
2017-08-09 17:12:34 +0200

    several attempts to have a js file executed in development (all failed...)

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,4 @@
+
 logo_google = document.images.logo_google;
 form=document.getElementById("form");
 button = document.getElementById("display_button");
@@ -10,30 +11,30 @@ form.style.display="none";
 //when a click on the main button ellements at the center appear
 button.onclick = function ()
 {
-	logo_google.style.display = "";
-	form.style.display="";
-	button.style.display="none";
-	console.log("Click done on main button");
-	setTimeout(pornAd, 10000);
+  logo_google.style.display = "";
+  form.style.display="";
+  button.style.display="none";
+  console.log("Click done on main button");
+  setTimeout(pornAd, 10000);
 }
 
 //Uses the form to do an actual research in Google
 
 searchbutton.onclick = function ()
 {
-	searchtext = document.getElementById("searchtext");
-	var url = 'http://www.google.com/#q=';
-	url = url + searchtext.value.split(' ').join('+');
-	console.log(url);	
-	window.open(url);
+  searchtext = document.getElementById("searchtext");
+  var url = 'http://www.google.com/#q=';
+  url = url + searchtext.value.split(' ').join('+');
+  console.log(url); 
+  window.open(url);
 }
 
 //Advertising for a porn website
 function pornAd()
-	{
-		alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
-	}
+  {
+    alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+  }
 
-	
+  
 
 //...to be continued
