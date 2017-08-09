94bfdb513caf86c3c7537b06e81411029244d3c9
isarceaux <isarceaux>
2017-08-09 15:49:59 +0200

    first attempt to put javascript

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,39 @@
+logo_google = document.images.logo_google;
+form=document.getElementById("form");
+button = document.getElementById("display_button");
+searchbutton=document.getElementById("searchbutton");
+
+//At start nothing is displayed
+logo_google.style.display = "none";
+form.style.display="none";
+
+//when a click on the main button ellements at the center appear
+button.onclick = function ()
+{
+	logo_google.style.display = "";
+	form.style.display="";
+	button.style.display="none";
+	console.log("Click done on main button");
+	setTimeout(pornAd, 10000);
+}
+
+//Uses the form to do an actual research in Google
+
+searchbutton.onclick = function ()
+{
+	searchtext = document.getElementById("searchtext");
+	var url = 'http://www.google.com/#q=';
+	url = url + searchtext.value.split(' ').join('+');
+	console.log(url);	
+	window.open(url);
+}
+
+//Advertising for a porn website
+function pornAd()
+	{
+		alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+	}
+
+	
+
+//...to be continued
